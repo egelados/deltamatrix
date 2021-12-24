aecfc3a4227a4d4b54c06bfc7a20f2c4440ae02e
Egelados Digit <egelados@gmail.com>
2021-12-24 10:41:10 +0000

    OK now i really fixed the styler bug goddamnit

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ data2['Delta'] = [-((l / m) - 1)*100 for l, m in zip(data2['Price'], data2['ATH'
 
 df2 = pd.DataFrame(data2)
 df3 = df2.sort_values(by=['Delta'])
-styler = df3.style.hide_index().format(subset=['Delta'], decimal=',', precision=4).bar(subset=['mean'], align="mid")
+styler = df3.style.hide_index().format(subset=['Delta'], decimal=',', precision=4).bar(subset=['Delta'], align="mid")
 
 st.table(styler.to_html(), unsafe_allow_html=True)
 #st.table(df2)
