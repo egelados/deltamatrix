b5136065a550f6bffbe9f56f5fc8c110e2c5d801
Egelados Digit <egelados@gmail.com>
2021-12-25 18:46:46 +0000

    changes to support multi denomination

--- main.py
+++ main.py
@@ -4,19 +4,18 @@ import pandas as pd
 
 st.write(""" # ATH Delta Matrix """)
 st.sidebar.header(""" Thx to @joed4lton from GCC for the idea. Had a few DANG moments preparing this :) """)
-agree = st.sidebar.checkbox('I agree')
-
-if agree:
+unit_of_denomination = st.sidebar.checkbox('Denominate in sats instead of fiat')
+if unit_of_denomination:
      st.write('Great!')
 
-def get_ticker(name):
-    coin = yf.Ticker(name)
+def get_ticker(name, unit):
+    coin = yf.Ticker(name + "-" + unit)
     return coin
 
-symbols = ["BTC-USD", "ETH-USD", "BNB-USD", "SOL-USD", "ADA-USD", 
-           "LUNA1-USD", "AVAX-USD", "DOT-USD", "DOGE-USD", "MANA-USD",
-           "SHIB-USD", "MATIC-USD", "CRO-USD", "UNI1-USD", "LTC-USD", 
-           "LINK-USD", "ALGO-USD", "ATOM-USD", "VET-USD", "EGLD-USD"]
+symbols = ["BTC", "ETH", "BNB", "SOL", "ADA", 
+           "LUNA1", "AVAX", "DOT", "DOGE", "MANA",
+           "SHIB", "MATIC", "CRO", "UNI1", "LTC", 
+           "LINK", "ALGO", "ATOM", "VET", "EGLD"]
 
 logos = [1, 1027, 1839, 5426, 2010, 
          4172, 5805, 6636, 74, 1966,
@@ -25,7 +24,7 @@ logos = [1, 1027, 1839, 5426, 2010,
 
 tickers = []
 for symbol in symbols:
-    tickers.append(get_ticker(symbol))
+    tickers.append(get_ticker(symbol,"USD"))
 
 histories = []
 for ticker in tickers:
